e0263563bf9ae93a92c5fe76d8a6230e3a786b43
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-29 18:55:18 +0200

    Send Request, Read Data and Create Users.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,13 @@
+import User from "../models/User.js";
 
-const createUser = (req,res) => {
-    res.json({msg: "Creando usuario"})
-}
+const createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const storedUsed = await user.save();
+    res.json(storedUsed);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export { createUser};
+export { createUser };
