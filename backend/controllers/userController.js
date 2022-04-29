003f0ee8ba58ab786835824a68eecceb465e9706
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-29 19:42:52 +0200

    Generate a unique token ID.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import generateId from "../helpers/generateId.js";
 
 const createUser = async (req, res) => {
   // Evitar registros duplicados
@@ -13,6 +14,7 @@ const createUser = async (req, res) => {
 
   try {
     const user = new User(req.body);
+    user.token = generateId();
     const storedUsed = await user.save();
     res.json(storedUsed);
   } catch (error) {
