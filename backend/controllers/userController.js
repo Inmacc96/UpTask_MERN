bcb2355510c75c832bed86f7711775811341af66
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-29 19:05:02 +0200

    Avoiding duplicate registrations.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,16 @@
 import User from "../models/User.js";
 
 const createUser = async (req, res) => {
+  // Evitar registros duplicados
+  const { email } = req.body;
+  const userExists = await User.findOne({
+    email,
+  });
+  if (userExists) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     const user = new User(req.body);
     const storedUsed = await user.save();
