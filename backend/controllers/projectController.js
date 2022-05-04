e8bbce663267eae9ca1a655ddd2c81fa9f19b8bd
inmacc96 <inmacaballerocarrero@gmail.com>
2022-05-04 11:50:06 +0200

    Get the authenticated user's projects.

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,12 +1,14 @@
 import Project from "../models/Project.js";
 
-const getProjects = async (req, res) => {};
+const getProjects = async (req, res) => {
+  const projects = await Project.find().where("creator").equals(req.user);
+
+  res.json(projects);
+};
 
 const newProject = async (req, res) => {
   const project = new Project(req.body);
   project.creator = req.user._id;
-
-  console.log(project)
 
   try {
     const projectStored = await project.save();
