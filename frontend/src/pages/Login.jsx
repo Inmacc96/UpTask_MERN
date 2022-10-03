eea45d1754d96e8e684756df3a340fcbe455d76e
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-03 18:35:55 +0200

    Improve the login form padding.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
         <span className="text-slate-700">proyectos</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
         <div className="my-5">
           <label
             htmlFor="email"
@@ -54,7 +54,7 @@ const Login = () => {
         >
           ¿No tienes una cuenta? Regístrate
         </Link>
-        
+
         <Link
           className=" block text-center my-5 text-slate-500 uppercase text-sm"
           to="/forget-password"
