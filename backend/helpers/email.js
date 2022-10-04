a43d6768b181707d58115f869e9e48d2f7fd070d
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-04 14:11:16 +0200

    Define function to send email confirm account.

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -1,6 +1,34 @@
 import nodemailer from "nodemailer";
 
 //Email para confirmar la cuenta
-export const emailConfirmAccount = (data) => {
-  console.log("DATOS", data);
+export const emailConfirmAccount = async (data) => {
+  const { email, name, token } = data;
+
+  //Configuramos el cliente que envia el email
+  const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "d8a613e505fdc4",
+      pass: "618ba8ae630bc3",
+    },
+  });
+
+  // Información del email
+
+  //Va a enviar el email una vez que identifica las credenciales de transport correctamente
+  const info = await transport.sendMail({
+    from: '"UpTask - Project Manager" <accounts@uptask.com>',
+    to: email,
+    subject: "UpTask - Confirm your account",
+    text: "Confirm your UpTask account",
+    html: `<p>Hi, ${name}. Confirm your UpTask account.</p>
+    <p>Your account is almost ready, just confirm it by clicking on the following link:
+
+    <a href="${process.env.FRONTEND_URL}/confirm/${token}">Confirm Account</a>
+
+    <p>If you did not create this account, you can ignore the message. </p>
+   
+    `
+  })
 };
