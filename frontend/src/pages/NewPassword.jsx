2112808c50cdd4a58a9bacc34ae8de90a2f3c884
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-06 15:39:40 +0200

    Validate the token to create new password and display error message in case of incorrect and in case of correct display the form.

--- frontend/src/pages/NewPassword.jsx
+++ frontend/src/pages/NewPassword.jsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import Alert from "../components/Alert";
 
 const NewPassword = () => {
+
+  const params = useParams();
+  const { token } = params;
+
+  const [isValidToken, setIsValidToken] = useState(false)
+  const [alert, setAlert] = useState({});
+
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        // TODO: Mover hacia un cliente axios
+        const url = `${import.meta.env.VITE_BACKEND_URL}/api/users/forget-password/${token}`
+        await axios(url);
+        setIsValidToken(true)
+      }
+      catch (err) {
+        setAlert({
+          msg: err.response.data.msg,
+          error: true
+        })
+      }
+    }
+    validateToken();
+  }, [])
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
@@ -7,7 +36,9 @@ const NewPassword = () => {
         <span className="text-slate-700">projects</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {alert.msg && <Alert alert={alert} />}
+
+      {isValidToken && (<form className="my-10 bg-white shadow rounded-lg p-10">
         <div className="my-5">
           <label
             htmlFor="password"
@@ -29,7 +60,7 @@ const NewPassword = () => {
           className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded
       hover:cursor-pointer hover:bg-sky-800 transition-colors"
         />
-      </form>
+      </form>)}
     </>
   )
 }
