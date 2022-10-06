b6db384e4a7d247d8905b85b9d0bc90788ad92f5
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-06 14:08:35 +0200

    Validation forgetPassword form.

--- frontend/src/pages/ForgetPassword.jsx
+++ frontend/src/pages/ForgetPassword.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alert from "../components/Alert";
 
 const ForgetPassword = () => {
+  const [email, setEmail] = useState("");
+
+  const [alert, setAlert] = useState({})
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validation
+    if (email === "" || email.length < 6) {
+      setAlert({
+        msg: "The email address is required",
+        error: true
+      })
+      return
+    }
+
+  }
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
@@ -8,7 +28,10 @@ const ForgetPassword = () => {
         <span className="text-slate-700">projects</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {alert.msg && <Alert alert={alert} />}
+
+      <form className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}>
         <div className="my-5">
           <label
             htmlFor="email"
@@ -21,6 +44,8 @@ const ForgetPassword = () => {
             type="email"
             placeholder="Registration Email"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
 
