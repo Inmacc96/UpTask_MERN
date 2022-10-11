403db8cc855825c017456156d65dd1094f61159d
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-11 13:56:05 +0200

    - Create the useProjects hook. - Add the projects state in ProjectsProvider.jsx

--- frontend/src/context/ProjectsProvider.jsx
+++ frontend/src/context/ProjectsProvider.jsx
@@ -5,8 +5,10 @@ const ProjectsContext = createContext();
 
 const ProjectsProvider = ({ children }) => {
 
+    const [projects, setProjects] = useState([]);
+
     return (
-        <ProjectsContext.Provider value={{}}>
+        <ProjectsContext.Provider value={{ projects }}>
             {children}
         </ProjectsContext.Provider>
     )
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,12 +1,18 @@
+import { Children } from "react";
+import useProjects from "../hooks/useProjects"
 
 const Projects = () => {
+    const { projects} = useProjects();
+
+    console.log(projects)
+
     return (
         <>
-        <h1 className="text-4xl font-black">Projects</h1>
+            <h1 className="text-4xl font-black">Projects</h1>
 
-        <div>
-            
-        </div>
+            <div>
+
+            </div>
         </>
     )
 }
