efc5f0dc2419ecda4a80fe271c9fa3251a63371a
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-11 19:13:33 +0200

    - Get the user's projects from the server. - Start defining the Projects component.

--- frontend/src/context/ProjectsProvider.jsx
+++ frontend/src/context/ProjectsProvider.jsx
@@ -11,6 +11,29 @@ const ProjectsProvider = ({ children }) => {
 
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const getProjects = async () => {
+            try {
+                const token = localStorage.getItem("token")
+                if (!token) return
+
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+
+                const { data } = await clientAxios("/projects", config)
+                setProjects(data)
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        getProjects();
+    }, [])
+
     const showAlert = alert => {
         setAlert(alert)
 
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -2,16 +2,18 @@ import { Children } from "react";
 import useProjects from "../hooks/useProjects"
 
 const Projects = () => {
-    const { projects} = useProjects();
-
-    console.log(projects)
+    const { projects } = useProjects();
 
     return (
         <>
             <h1 className="text-4xl font-black">Projects</h1>
 
-            <div>
-
+            <div className="bg-white shadow mt-10 rounded-lg p-5">
+                {projects.length ?
+                    <p>There are projects</p> :
+                    <p className=" text-center text-gray-600 uppercase">
+                        No projects yet
+                    </p>}
             </div>
         </>
     )
