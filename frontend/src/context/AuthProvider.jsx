b35d14debf42151ea36a7f76c4951cf10b9d4e4c
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-11 12:18:46 +0200

    Send token to the server to authenticate the user.

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,10 +1,37 @@
 import { useState, useEffect, createContext } from "react";
+import clientAxios from "../config/clientAxios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
 
     const [auth, setAuth] = useState({});
+
+    useEffect(() => {
+        const authUser = async () => {
+            const token = localStorage.getItem("token")
+            if (!token) {
+                return
+            }
+            else {
+
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+                try {
+                    const { data } = await clientAxios("/users/profile", config)
+                    setAuth(data);
+
+                } catch (err) {
+
+                }
+            }
+        }
+        authUser()
+    }, [])
 
 
     return (
