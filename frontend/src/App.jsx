08bb79b0d207f1d5de34fd387661ba6a20320223
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-11 11:33:33 +0200

    Create authentication context.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,23 @@ import ForgetPassword from "./pages/ForgetPassword";
 import NewPassword from "./pages/NewPassword";
 import ConfirmAccount from "./pages/ConfirmAccount";
 
+import { AuthProvider } from "./context/AuthProvider"
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Esta va a ser el área pública */}
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="forget-password" element={<ForgetPassword />} />
-          <Route path="forget-password/:token" element={<NewPassword />} />
-          <Route path="confirm/:token" element={<ConfirmAccount />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          {/* Esta va a ser el área pública */}
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="forget-password" element={<ForgetPassword />} />
+            <Route path="forget-password/:token" element={<NewPassword />} />
+            <Route path="confirm/:token" element={<ConfirmAccount />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
