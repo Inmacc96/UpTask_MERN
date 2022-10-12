8b450a3f3770e53d2bba3c5de6ba904a7f253879
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-12 18:13:15 +0200

    Create the task form.

--- frontend/src/components/ModalFormTasks.jsx
+++ frontend/src/components/ModalFormTasks.jsx
@@ -2,8 +2,13 @@ import { Fragment, useState, useEffect } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import useProjects from '../hooks/useProjects'
 
+const PRIORITY = ["Low", "Medium", "High"]
 
 const ModalFormTasks = () => {
+
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [priority, setPriority] = useState("")
 
     const { modalFormTasks, handleModalTask } = useProjects();
 
@@ -59,16 +64,72 @@ const ModalFormTasks = () => {
                             <div className="sm:flex sm:items-start">
                                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
                                     <Dialog.Title as="h3" className="text-lg leading-6 font-bold text-gray-900">
-
+                                        Create Task
                                     </Dialog.Title>
 
+                                    <form className='my-10'>
+                                        <div className='mb-5'>
+                                            <label
+                                                className='text-gray-700 uppercase font-bold text-sm'
+                                                htmlFor='name'>
+                                                Task Name
+                                            </label>
+                                            <input
+                                                id="name"
+                                                type="text"
+                                                placeholder="Name of task"
+                                                className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                                                value={name}
+                                                onChange={e => setName(e.target.value)} />
+                                        </div>
+
+                                        <div className='mb-5'>
+                                            <label
+                                                className='text-gray-700 uppercase font-bold text-sm'
+                                                htmlFor='desctiption'>
+                                                Task Description
+                                            </label>
+                                            <textarea
+                                                id="description"
+                                                placeholder="Description of task"
+                                                className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                                                value={description}
+                                                onChange={e => setDescription(e.target.value)} />
+                                        </div>
+
+                                        <div className='mb-5'>
+                                            <label
+                                                className='text-gray-700 uppercase font-bold text-sm'
+                                                htmlFor='priority'>
+                                                Priority
+                                            </label>
+                                            <select
+                                                id="priority"
+                                                className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                                                value={priority}
+                                                onChange={e => setPriority(e.target.value)} >
+                                                <option value="">-- Select Priority ---</option>
+
+                                                {PRIORITY.map(option => (
+                                                    <option key={option} value={option}>{option}</option>
+                                                ))}
+                                            </select>
+                                        </div>
+
+                                        <input
+                                            type="submit"
+                                            className='text-sm bg-sky-600 hover:bg-sky-700 p-3 w-full text-white uppercase font-bold cursor-pointer transition-colors rounded'
+                                            value="Create Task"
+                                        />
+
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </Transition.Child>
-                </div>
-            </Dialog>
-        </Transition.Root>
+                </div >
+            </Dialog >
+        </Transition.Root >
     )
 }
 
